fff514ac54b6fef8e184e8c4413ac92d18caa143
Nathan Witmer <nathan@zerowidth.com>
2017-03-23 14:35:21 -0600

    Pull comments into test cases themselves for better error output

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,52 +7,51 @@ import (
 var repoMap = map[string]string{
 	"df":  "zerowidth/dotfiles",
 	"df2": "zerowidth/dotfiles2", // prefix collision
-	"lg2": "libgit2/libgit2",
 }
 
 func TestParse(t *testing.T) {
 	repoTests := []struct {
-		input string // the input
-		repo  string // the expected repo match or expansion
+		input   string // the input
+		repo    string // the expected repo match or expansion
+		example string // what the case is an example of
 	}{
-		{"", ""},                     // no input, no repo match
-		{"df", "zerowidth/dotfiles"}, // match shorthand
-		{" df", ""},                  // no match, leading space
-		{"foo/bar", "foo/bar"},       // fully qualified
+		{"", "", "no input, no repo match"},
+		{"df", "zerowidth/dotfiles", "match shorthand"},
+		{" df", "", "no match, leading space"},
+		{"foo/bar", "foo/bar", "fully qualified repo name"},
 	}
 
 	repoIssueTests := []struct {
-		input string // the input
-		repo  string // the expected repo match or expansion
-		issue string // the expected issue match
+		input   string // the input
+		repo    string // the expected repo match or expansion
+		issue   string // the expected issue match
+		example string // what the case is an example of
 	}{
-		{"", "", ""}, // no issue nor repo
-		{"df 123", "zerowidth/dotfiles", "123"},
-		{"df#123", "zerowidth/dotfiles", "123"},
-		{"df #123", "zerowidth/dotfiles", "123"}, // space and hash
-		{"df123", "zerowidth/dotfiles", "123"},   // prefix match
-		{"df2 34", "zerowidth/dotfiles2", "34"},
-		{"df234", "zerowidth/dotfiles2", "34"}, // prefix match
-		{"lg2 123", "libgit2/libgit2", "123"},
-		{"lg2123", "libgit2/libgit2", "123"},  // prefix match
-		{"foo/bar 123", "foo/bar", "123"},     // fully qualified
-		{"df 0123", "zerowidth/dotfiles", ""}, // invalid issue
+		{"", "", "", "no issue no repo"},
+		{"df 123", "zerowidth/dotfiles", "123", "normal expansion"},
+		{"df#123", "zerowidth/dotfiles", "123", "expansion with #"},
+		{"df #123", "zerowidth/dotfiles", "123", "space and # both"},
+		{"df123", "zerowidth/dotfiles", "123", "prefix match"},
+		{"df2 34", "zerowidth/dotfiles2", "34", "numeric suffix on shorthand"},
+		{"df234", "zerowidth/dotfiles2", "34", "numerix suffix with no space"},
+		{"foo/bar 123", "foo/bar", "123", "fully qualified repo"},
+		{"df 0123", "zerowidth/dotfiles", "", "invalid issue"},
 	}
 
 	for _, tc := range repoTests {
 		result := Parse(repoMap, tc.input)
 		if result.Repo != tc.repo {
-			t.Errorf("input %#v: expected repo %#v, got %#v", tc.input, tc.repo, result.Repo)
+			t.Errorf("Parse(%#v): %s: expected repo %#v, got %#v", tc.input, tc.example, tc.repo, result.Repo)
 		}
 	}
 
 	for _, tc := range repoIssueTests {
 		result := Parse(repoMap, tc.input)
 		if result.Repo != tc.repo {
-			t.Errorf("input %#v: expected repo %#v, got %#v", tc.input, tc.repo, result.Repo)
+			t.Errorf("Parse(%#v): %s: expected repo %#v, got %#v", tc.input, tc.example, tc.repo, result.Repo)
 		}
 		if result.Issue != tc.issue {
-			t.Errorf("input %#v: expected issue %#v, got %#v", tc.input, tc.issue, result.Issue)
+			t.Errorf("Parse(%#v): %s: expected issue %#v, got %#v", tc.input, tc.example, tc.issue, result.Issue)
 		}
 	}
 
