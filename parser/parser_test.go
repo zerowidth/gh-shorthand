23cb552fa1a889c39bd41110b42c9b7be42aba03
Nathan Witmer <nathan@zerowidth.com>
2017-03-27 19:57:39 -0600

    Rearrange field ordering in parse tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -12,138 +12,138 @@ var repoMap = map[string]string{
 
 func TestParse(t *testing.T) {
 	repoTests := []struct {
+		desc  string // description of the test case
 		input string // the input
 		repo  string // the expected repo match or expansion
-		issue string // the expected issue match
 		match string // the matched repo shorthand
+		issue string // the expected issue match
 		query string // the remaining query text after parsing/expansion
-		desc  string // description of the test case
 	}{
 		{
-			input: "",
 			desc:  "no issue, no repo",
+			input: "",
 		},
 		{
+			desc:  "shorthand match",
 			input: "df",
 			repo:  "zerowidth/dotfiles",
 			match: "df",
-			desc:  "shorthand match",
 		},
 		{
+			desc:  "no match, leading space",
 			input: " df",
 			repo:  "",
 			match: "",
 			query: " df",
-			desc:  "no match, leading space",
 		},
 		{
+			desc:  "fully qualified repo name",
 			input: "foo/bar",
 			repo:  "foo/bar",
 			match: "",
-			desc:  "fully qualified repo name",
 		},
 		{
+			desc:  "normal expansion",
 			input: "df 123",
 			repo:  "zerowidth/dotfiles",
 			issue: "123",
 			match: "df",
-			desc:  "normal expansion",
 		},
 		{
+			desc:  "expansion with #",
 			input: "df#123",
 			repo:  "zerowidth/dotfiles",
 			issue: "123",
 			match: "df",
-			desc:  "expansion with #",
 		},
 		{
+			desc:  "space and # both",
 			input: "df #123",
 			repo:  "zerowidth/dotfiles",
 			issue: "123",
 			match: "df",
-			desc:  "space and # both",
 		},
 		{
+			desc:  "prefix match",
 			input: "df123",
 			repo:  "zerowidth/dotfiles",
 			issue: "123",
 			match: "df",
-			desc:  "prefix match",
 		},
 		{
+			desc:  "single digit issue",
 			input: "df 1",
 			repo:  "zerowidth/dotfiles",
 			issue: "1",
 			match: "df",
-			desc:  "single digit issue",
 		},
 		{
+			desc:  "numeric suffix on match",
 			input: "df2 34",
 			repo:  "zerowidth/dotfiles2",
 			issue: "34",
 			match: "df2",
-			desc:  "numeric suffix on match",
 		},
 		{
+			desc:  "numerix suffix with no space",
 			input: "df234",
 			repo:  "zerowidth/dotfiles2",
 			issue: "34",
 			match: "df2",
-			desc:  "numerix suffix with no space",
 		},
 		{
+			desc:  "fully qualified repo",
 			input: "foo/bar 123",
 			repo:  "foo/bar",
 			issue: "123",
 			match: "",
-			desc:  "fully qualified repo",
 		},
 		{
+			desc:  "invalid issue",
 			input: "df 0123",
 			repo:  "zerowidth/dotfiles",
 			issue: "",
 			match: "df",
 			query: "0123",
-			desc:  "invalid issue",
 		},
 		{
+			desc:  "retrieve query after expansion",
 			input: "df foo",
 			repo:  "zerowidth/dotfiles",
 			match: "df",
 			query: "foo",
-			desc:  "retrieve query after expansion",
 		},
 		{
+			desc:  "treats unparsed issue as query",
 			input: "df 123 foo",
 			repo:  "zerowidth/dotfiles",
 			issue: "",
 			match: "df",
 			query: "123 foo",
-			desc:  "treats unparsed issue as query",
 		},
 		{
+			desc:  "treats issue with any other text as a query",
 			input: "123 foo",
 			issue: "",
 			query: "123 foo",
-			desc:  "treats issue with any other text as a query",
 		},
 		{
+			desc:  "retrieve query",
 			input: "foo bar",
 			query: "foo bar",
-			desc:  "retrieve query",
 		},
 		{
+			desc:  "ignores whitespace after shorthand",
 			input: "df ",
 			repo:  "zerowidth/dotfiles",
 			match: "df",
 			query: "",
-			desc:  "ignores whitespace after shorthand",
 		},
 		{
+			desc:  "ignores whitespace after repo",
 			input: "foo/bar ",
 			repo:  "foo/bar",
 			query: "",
-			desc:  "ignores whitespace after repo",
 		},
 	}
 
@@ -151,18 +151,17 @@ func TestParse(t *testing.T) {
 		t.Run(fmt.Sprintf("Parse(%#v): %s", tc.input, tc.desc), func(t *testing.T) {
 			result := Parse(repoMap, tc.input)
 			if result.Repo != tc.repo {
-				t.Errorf("expected repo %#v, got %#v", tc.repo, result.Repo)
-			}
-			if result.Issue != tc.issue {
-				t.Errorf("expected issue %#v, got %#v", tc.issue, result.Issue)
+				t.Errorf("expected Repo %#v, got %#v", tc.repo, result.Repo)
 			}
 			if result.Match != tc.match {
-				t.Errorf("expected match %#v, got %#v", tc.match, result.Match)
+				t.Errorf("expected Match %#v, got %#v", tc.match, result.Match)
+			}
+			if result.Issue != tc.issue {
+				t.Errorf("expected Issue %#v, got %#v", tc.issue, result.Issue)
 			}
 			if result.Query != tc.query {
-				t.Errorf("expected query %#v, got %#v", tc.query, result.Query)
+				t.Errorf("expected Query %#v, got %#v", tc.query, result.Query)
 			}
-
 		})
 	}
 
