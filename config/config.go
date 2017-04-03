7714ab1c5e93bdc963435c49fccd3f1962aee2ee
Nathan Witmer <nathan@zerowidth.com>
2017-04-03 12:10:28 -0600

    Add ProjectDirs to Config, and normalize the paths

--- config/config.go
+++ config/config.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"io/ioutil"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
 
 	"gopkg.in/yaml.v2"
 )
@@ -10,6 +13,7 @@ import (
 type Config struct {
 	RepoMap     map[string]string `yaml:"repos"`
 	DefaultRepo string            `yaml:"default_repo"`
+	ProjectDirs []string          `yaml:"project_dirs"`
 }
 
 // Load a Config from a yaml string.
@@ -19,6 +23,22 @@ func Load(yml string) (*Config, error) {
 	if err := yaml.Unmarshal([]byte(yml), &config); err != nil {
 		return nil, err
 	}
+
+	// normalize the project directories
+	paths := make([]string, len(config.ProjectDirs))
+	for i, path := range config.ProjectDirs {
+		expanded, err := homedir.Expand(path)
+		if err != nil {
+			return nil, err
+		}
+		absolute, err := filepath.Abs(expanded)
+		if err != nil {
+			return nil, err
+		}
+		paths[i] = absolute
+	}
+	config.ProjectDirs = paths
+
 	return &config, nil
 }
 
--- config/config_test.go
+++ config/config_test.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"path/filepath"
 	"reflect"
 	"testing"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 var (
@@ -10,6 +13,10 @@ var (
 repos:
   df: zerowidth/dotfiles
 default_repo: zerowidth/default
+project_dirs:
+  - /foo/foo
+  - ~/bar
+  - relative
 `
 	invalidYaml = "---\nrepos: []"
 
@@ -19,18 +26,44 @@ default_repo: zerowidth/default
 )
 
 func TestLoad(t *testing.T) {
+	config, err := Load(configYaml)
+	if err != nil {
+		t.Errorf("could not load config yaml: %q", err.Error())
+	} else {
+		if !reflect.DeepEqual(config.RepoMap, repoMap) {
+			t.Errorf("expected RepoMap to be %#v, got %#v", repoMap, config.RepoMap)
+		}
+
+		if config.DefaultRepo != "zerowidth/default" {
+			t.Errorf("expected DefaultRepo to be %q, got %q", "zerowidth/default", config.DefaultRepo)
+		}
+
+		if _, err := Load(invalidYaml); err == nil {
+			t.Error("expected invalid YML to error, but no error occurred")
+		}
+	}
+}
+
+func TestLoadProjectDirs(t *testing.T) {
 	config, _ := Load(configYaml)
-	if !reflect.DeepEqual(config.RepoMap, repoMap) {
-		t.Errorf("expected RepoMap to be %#v, got %#v", repoMap, config.RepoMap)
+	if len(config.ProjectDirs) != 3 {
+		t.Errorf("expected ProjectDirs to have three entries, got %#v", config.ProjectDirs)
+	} else {
+		barPath, err := homedir.Expand("~/bar")
+		if err != nil {
+			t.Errorf("error expanding ~/bar: %#v", err.Error())
+		}
+		if config.ProjectDirs[0] != "/foo/foo" {
+			t.Errorf("expected first project dir to be /foo/foo, got %#v", config.ProjectDirs[0])
+		}
+		if config.ProjectDirs[1] != barPath {
+			t.Errorf("expected ~/bar to be expanded, got %#v", config.ProjectDirs[1])
+		}
+		if !filepath.IsAbs(config.ProjectDirs[2]) {
+			t.Errorf("expected relative path to be expanded to absolute path, got %#v", config.ProjectDirs[2])
+		}
 	}
 
-	if config.DefaultRepo != "zerowidth/default" {
-		t.Errorf("expected DefaultRepo to be %q, got %q", "zerowidth/default", config.DefaultRepo)
-	}
-
-	if _, err := Load(invalidYaml); err == nil {
-		t.Error("expected invalid YML to error, but no error occurred")
-	}
 }
 
 func TestLoadFromFile(t *testing.T) {
