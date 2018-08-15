709bb118a24790a1dda53ed6c85dc3d8a7383e9f
Nathan Witmer <nathan@zerowidth.com>
2018-08-14 20:43:26 -0600

    Rework server to use service.Service instead, and add server commands

--- cmd/gh-shorthand.go
+++ cmd/gh-shorthand.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/kardianos/service"
 	"github.com/spf13/cobra"
 	"github.com/zerowidth/gh-shorthand/internal/pkg/completion"
 	"github.com/zerowidth/gh-shorthand/internal/pkg/config"
@@ -41,18 +42,6 @@ var completeCommand = &cobra.Command{
 	},
 }
 
-var serverCommand = &cobra.Command{
-	Use: "server",
-	Run: func(cmd *cobra.Command, args []string) {
-
-		cfg, err := config.LoadFromFile(config.Filename)
-		if err != nil {
-			log.Fatal("couldn't load config", err)
-		}
-		server.Run(cfg)
-	},
-}
-
 var markdownCommand = &cobra.Command{
 	Use: "markdown-link",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -69,11 +58,99 @@ var issueReferenceCommand = &cobra.Command{
 	},
 }
 
+var serverCommand = &cobra.Command{
+	Use:   "server",
+	Short: "Run or manage a gh-shorthand server",
+}
+
+var serverRun = &cobra.Command{
+	Use:   "run",
+	Short: "Run a gh-shorthand server in the foreground",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadFromDefault()
+		svc := server.Service(cfg)
+		err := svc.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+var serverInstall = &cobra.Command{
+	Use:   "install",
+	Short: "Install the gh-shorthand server",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadFromDefault()
+		svc := server.Service(cfg)
+		err := service.Control(svc, "install")
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+var serverRemove = &cobra.Command{
+	Use:   "remove",
+	Short: "Remove the gh-shorthand server",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadFromDefault()
+		svc := server.Service(cfg)
+		err := service.Control(svc, "uninstall")
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+var serverStart = &cobra.Command{
+	Use:   "start",
+	Short: "Start the gh-shorthand server in the background",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadFromDefault()
+		svc := server.Service(cfg)
+		err := service.Control(svc, "start")
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+var serverStop = &cobra.Command{
+	Use:   "stop",
+	Short: "Stop the gh-shorthand server",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadFromDefault()
+		svc := server.Service(cfg)
+		err := service.Control(svc, "stop")
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+var serverRestart = &cobra.Command{
+	Use:   "restart",
+	Short: "Restart the gh-shorthand server",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.LoadFromDefault()
+		svc := server.Service(cfg)
+		err := service.Control(svc, "restart")
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(completeCommand)
 	rootCmd.AddCommand(serverCommand)
 	rootCmd.AddCommand(markdownCommand)
 	rootCmd.AddCommand(issueReferenceCommand)
+
+	serverCommand.AddCommand(serverRun)
+	serverCommand.AddCommand(serverInstall)
+	serverCommand.AddCommand(serverRemove)
+	serverCommand.AddCommand(serverStart)
+	serverCommand.AddCommand(serverStop)
+	serverCommand.AddCommand(serverRestart)
 }
 
 func main() {
--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -2,34 +2,79 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/kardianos/service"
 	"github.com/zerowidth/gh-shorthand/internal/pkg/config"
 	"github.com/zerowidth/gh-shorthand/internal/pkg/rpc"
 )
 
-// Run runs the gh-shorthand RPC server on the configured unix socket path
-func Run(cfg config.Config) {
-	if len(cfg.SocketPath) == 0 {
-		log.Fatalf("no socket_path configured in %s", config.Filename)
+type server struct {
+	cfg  config.Config
+	stop chan interface{}
+	exit chan interface{}
+}
+
+// Service returns a service for this rpc server.
+func Service(cfg config.Config) service.Service {
+	sc := service.Config{
+		Name:        "gh-shorthand",
+		DisplayName: "gh-shorthand",
+		Description: "GitHub autocompletion tools for Alfred",
+		Arguments:   []string{"server", "run"},
+		Option: service.KeyValue{
+			"UserService": true, // run as current user, not root
+			"RunAtLoad":   true, // run at boot
+		},
 	}
 
-	if len(cfg.APIToken) == 0 {
-		log.Fatalf("no api_token configured in %s", config.Filename)
+	server := server{
+		cfg:  cfg,
+		stop: make(chan interface{}), // stop the server
+		exit: make(chan interface{}), // server's ready to exit
 	}
 
+	svc, err := service.New(&server, &sc)
+	if err != nil {
+		log.Fatalf("couldn't create daemon: %s", err.Error())
+	}
+	return svc
+}
+
+func (s *server) Start(service.Service) error {
+	if len(s.cfg.SocketPath) == 0 {
+		return fmt.Errorf("no socket_path configured in %s", config.Filename)
+	}
+
+	if len(s.cfg.APIToken) == 0 {
+		return fmt.Errorf("no api_token configured in %s", config.Filename)
+	}
+
+	go s.run()
+
+	return nil
+}
+
+func (s *server) Stop(service.Service) error {
+	// these two channels allows the main goroutine to shut down cleanly.
+	close(s.stop)
+	<-s.exit
+	return nil
+}
+
+// run the gh-shorthand RPC server on the configured unix socket path
+func (s *server) run() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	h := rpc.NewHandler(cfg)
+	h := rpc.NewHandler(s.cfg)
 	h.Mount(r)
 
 	server := &http.Server{
@@ -38,19 +83,16 @@ func Run(cfg config.Config) {
 		WriteTimeout: time.Second,
 	}
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-
-	sock, err := net.Listen("unix", cfg.SocketPath)
+	sock, err := net.Listen("unix", s.cfg.SocketPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer func() {
-		os.Remove(cfg.SocketPath)
+		os.Remove(s.cfg.SocketPath)
 	}()
 
 	go func() {
-		log.Printf("server started on %s\n", cfg.SocketPath)
+		log.Printf("server started on %s\n", s.cfg.SocketPath)
 		if err := server.Serve(sock); err != nil {
 			if err != http.ErrServerClosed {
 				log.Fatal("server error", err)
@@ -58,7 +100,8 @@ func Run(cfg config.Config) {
 		}
 	}()
 
-	<-sig
+	// wait for service to be stopped
+	<-s.stop
 
 	log.Printf("shutting down server")
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
@@ -67,4 +110,6 @@ func Run(cfg config.Config) {
 	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal("server shutdown error", err)
 	}
+
+	close(s.exit) // signal service.Stop that we're done
 }
