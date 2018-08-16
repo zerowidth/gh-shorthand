1541d244d09598bdf3d381dcd56dc717c6427df8
Nathan Witmer <nathan@zerowidth.com>
2018-08-16 12:25:36 -0600

    Switch to service logger, which can use syslog facility

--- internal/pkg/server/server.go
+++ internal/pkg/server/server.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -18,12 +20,18 @@ import (
 
 type server struct {
 	cfg  config.Config
-	stop chan interface{}
-	exit chan interface{}
+	stop chan interface{} // external "stop the server" signal
+	done chan interface{} // internal "all done, exit" signal
 }
 
 // Service returns a service for this rpc server.
 func Service(cfg config.Config) service.Service {
+	server := server{
+		cfg:  cfg,
+		stop: make(chan interface{}),
+		done: make(chan interface{}),
+	}
+
 	sc := service.Config{
 		Name:        "gh-shorthand",
 		DisplayName: "gh-shorthand",
@@ -32,13 +40,18 @@ func Service(cfg config.Config) service.Service {
 		Option: service.KeyValue{
 			"UserService": true, // run as current user, not root
 			"RunAtLoad":   true, // run at boot
-		},
-	}
 
-	server := server{
-		cfg:  cfg,
-		stop: make(chan interface{}), // stop the server
-		exit: make(chan interface{}), // server's ready to exit
+			// override the default runwait to select on server.err channel, so an
+			// error from within the server will terminate the service.
+			"RunWait": func() {
+				var sigChan = make(chan os.Signal, 3)
+				signal.Notify(sigChan, syscall.SIGTERM, os.Interrupt)
+				select {
+				case <-sigChan:
+				case <-server.done:
+				}
+			},
+		},
 	}
 
 	svc, err := service.New(&server, &sc)
@@ -48,7 +61,7 @@ func Service(cfg config.Config) service.Service {
 	return svc
 }
 
-func (s *server) Start(service.Service) error {
+func (s *server) Start(svc service.Service) error {
 	if len(s.cfg.SocketPath) == 0 {
 		return fmt.Errorf("no socket_path configured in %s", config.Filename)
 	}
@@ -57,24 +70,47 @@ func (s *server) Start(service.Service) error {
 		return fmt.Errorf("no api_token configured in %s", config.Filename)
 	}
 
-	go s.run()
+	logger, err := svc.Logger(nil)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		if err := s.run(logger); err != nil {
+			close(s.done)
+		}
+	}()
 
 	return nil
 }
 
 func (s *server) Stop(service.Service) error {
-	// these two channels allows the main goroutine to shut down cleanly.
 	close(s.stop)
-	<-s.exit
+	// let logs from the goroutine make it through:
+	<-time.After(100 * time.Millisecond)
 	return nil
 }
 
-// run the gh-shorthand RPC server on the configured unix socket path
-func (s *server) run() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+// struct to wrap a service logger with an interface for middleware to use
+type mwLogger struct {
+	logger service.Logger
+}
 
-	h := rpc.NewHandler(s.cfg)
+func (ml mwLogger) Print(v ...interface{}) {
+	_ = ml.logger.Info(v...)
+}
+
+// run the gh-shorthand RPC server on the configured unix socket path
+func (s *server) run(logger service.Logger) error {
+	r := chi.NewRouter()
+
+	formatter := middleware.DefaultLogFormatter{
+		Logger:  mwLogger{logger: logger},
+		NoColor: !service.Interactive(), // double negative! interactive means color.
+	}
+	r.Use(middleware.RequestLogger(&formatter))
+
+	h := rpc.NewHandler(s.cfg, logger)
 	h.Mount(r)
 
 	server := &http.Server{
@@ -85,31 +121,34 @@ func (s *server) run() {
 
 	sock, err := net.Listen("unix", s.cfg.SocketPath)
 	if err != nil {
-		log.Fatal(err)
+		_ = logger.Error(err)
+		return err
 	}
 	defer func() {
 		os.Remove(s.cfg.SocketPath)
 	}()
 
 	go func() {
-		log.Printf("server started on %s\n", s.cfg.SocketPath)
+		_ = logger.Infof("server started on %s\n", s.cfg.SocketPath)
 		if err := server.Serve(sock); err != nil {
 			if err != http.ErrServerClosed {
-				log.Fatal("server error", err)
+				_ = logger.Error("server error", err)
+				return
 			}
 		}
+		defer close(s.done)
 	}()
 
 	// wait for service to be stopped
 	<-s.stop
 
-	log.Printf("shutting down server")
+	_ = logger.Infof("shutting down server")
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
-		log.Fatal("server shutdown error", err)
+		_ = logger.Error("server shutdown error", err)
 	}
 
-	close(s.exit) // signal service.Stop that we're done
+	return nil
 }
