bc5944d6852f63939c719cba19e84bb46bdc761a
Nathan Witmer <nathan@zerowidth.com>
2020-02-26 22:30:05 -0700

    Use filepath.Glob to support globs in project directory searches
    This allows for project dir configs such as "~/go/src/github.com/*" to
    pull in every available github-namespaced golang package directory.

--- pkg/completion/project_dir_items_test.go
+++ pkg/completion/project_dir_items_test.go
@@ -5,17 +5,28 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFindProjectDirs(t *testing.T) {
 	assert := assert.New(t)
 
-	fixturePath, _ := filepath.Abs("testdata/projects")
-	dirs, err := findProjectDirs(fixturePath)
+	dirs, err := findProjectDirs("testdata/projects")
 	assert.NoError(err)
-	assert.Contains(dirs, "project-bar", "normal directory in\n%v", dirs)
-	assert.Contains(dirs, "linked", "symlinked directory in\n%v", dirs)
-	assert.NotContains(dirs, "linked-file", "symlinked file in\n%v", dirs)
+	assert.Contains(dirs, "testdata/projects/project-bar", "normal directory in\n%v", dirs)
+	assert.Contains(dirs, "testdata/projects/linked", "symlinked directory in\n%v", dirs)
+	assert.NotContains(dirs, "testdata/projects/linked-file", "symlinked file in\n%v", dirs)
+}
+
+func TestFindProjectDirsGlob(t *testing.T) {
+	assert := assert.New(t)
+
+	dirs, err := findProjectDirs("testdata/*")
+	assert.NoError(err)
+	assert.Contains(dirs, "testdata/work/work-foo", "normal directory in\n%v", dirs)
+	assert.Contains(dirs, "testdata/projects/project-bar", "normal directory in\n%v", dirs)
+	assert.Contains(dirs, "testdata/projects/linked", "symlinked directory in\n%v", dirs)
+	assert.NotContains(dirs, "testdata/projects/linked-file", "symlinked file in\n%v", dirs)
 }
 
 func TestFindInvalidProjectDirs(t *testing.T) {
@@ -28,4 +39,22 @@ func TestFindInvalidProjectDirs(t *testing.T) {
 	if assert.Error(err) {
 		assert.Contains(err.Error(), "no such file")
 	}
+}
+
+func TestProjectDirItemsWithFuzzySearch(t *testing.T) {
+	fixturePath, err := filepath.Abs("testdata")
+	require.NoError(t, err)
+	dirs := projectDirItems([]string{"testdata/projects"}, "tdprojbar", modeEdit)
+	require.Len(t, dirs, 1)
+	assert.Equal(t, fixturePath+"/projects/project-bar", dirs[0].Arg)
+}
+
+func TestProjectDirItemsWithGlob(t *testing.T) {
+	fixturePath, err := filepath.Abs("testdata")
+	require.NoError(t, err)
+	dirs := projectDirItems([]string{fixturePath + "/*"}, "", modeEdit)
+	require.Len(t, dirs, 3)
+	dirs = projectDirItems([]string{fixturePath + "/w*"}, "", modeEdit)
+	require.Len(t, dirs, 1)
+	assert.Equal(t, fixturePath+"/work/work-foo", dirs[0].Arg)
 }
